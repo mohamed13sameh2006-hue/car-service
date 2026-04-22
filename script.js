dea94bc4f28c71a1b2e9db0933f25c877da23bb1
ms <mohamed13sameh2006@gmail.com>
2026-04-22 23:49:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 
-document.getElementById("bookingForm").addEventListener("submit", function (e) {
-    e.preventDefault();
+function sendToWhatsApp() {
 
     let name = document.getElementById("name").value;
     let phone = document.getElementById("phone").value;
+    let car = document.getElementById("car").value;
     let service = document.getElementById("service").value;
     let date = document.getElementById("date").value;
     let time = document.getElementById("time").value;
 
-    let message = `🚗 حجز صيانة
-👤 الاسم: ${name}
-📞 الرقم: ${phone}
-🔧 الخدمة: ${service}
-📅 التاريخ: ${date}
-⏰ الوقت: ${time}`;
+    let message = `New Booking:
+Name: ${name}
+Phone: ${phone}
+Car: ${car}
+Service: ${service}
+Date: ${date}
+Time: ${time}`;
 
-    let whatsappNumber = "201070656214";
-
-    let url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
+    let url = "https://wa.me/201070383775?text=" + encodeURIComponent(message);
 
     window.open(url, "_blank");
-});
+}
